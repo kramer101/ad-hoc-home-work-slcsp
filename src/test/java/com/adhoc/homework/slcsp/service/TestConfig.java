f58528be0c9cc16f894ab5fc12626e8975ffd0dd
Vadym_Yakovlev <vadym_yakovlev@icloud.com>
2023-03-06 10:55:07 -0800

    fixing junit test invocation

--- src/test/java/com/adhoc/homework/slcsp/service/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.adhoc.homework.slcsp.service;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles("test")
-@TestConfiguration
-public class TestConfig {
-}
